213f2aba9b4ec65bb6afbadc8fdf5d76b27051e8
elijah-schow <elijah.schow@gmail.com>
2017-02-17 11:23:38 -0600

    Implement grocerylist components

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// TODO
+var App = () =>  (
+  <h2>Grocery List: </h2>
+  <GroceryList />
+);
+
+var GroceryList = (attr) => (
+  <ul> 
+    {attr.groceries.map( grocery => 
+      <GroceryListItem name={grocery} />
+    )}
+  </ul>
+);
+
+var GroceryListItem = (attr) => (
+  <li>{attr.name}</li>
+);
+
+
+
+
